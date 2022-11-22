1f26b10b475a882d571dfaffab3cd8134926e688
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-22 19:27:49 +0700

    create user order

---
+++ LAOS-LP-API/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LAOS_LP_API.Models
+{
+    public class User
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public bool is_admin { get; set; }
+    }
+}
