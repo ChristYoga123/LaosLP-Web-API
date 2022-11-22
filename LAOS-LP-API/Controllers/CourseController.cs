666b0e18594965a494e212389b6b620383733304
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-22 16:44:19 +0700

    create course controller

---
+++ LAOS-LP-API/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using LAOS_LP_API.Data;
+using LAOS_LP_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LAOS_LP_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public CourseController(ApplicationDbContext context) => _context = context;
+        
+        [HttpGet]
+        public IEnumerable<Course> Get()
+        {
+            var course = _context.courses.Include(ct => ct.Category);
+            return course.ToList();
+        }
+
+    }
+}
