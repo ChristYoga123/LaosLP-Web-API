16951648079ffe300b51fdd00679deeb28af933a
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-22 16:38:50 +0700

    create category api

---
+++ LAOS-LP-API/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using LAOS_LP_API.Data;
+using LAOS_LP_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LAOS_LP_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context) => _context = context;
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+            return _context.categories.ToList();
+        }
+
+    }
+}
