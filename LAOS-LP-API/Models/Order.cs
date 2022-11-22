4c9365c1a69bab14d49603babc0756b19e03d1d3
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-22 19:27:41 +0700

    create order model

---
+++ LAOS-LP-API/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LAOS_LP_API.Models
+{
+    public class Order
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        [ForeignKey("User")]
+        public int user_id { get; set; }
+        public User? User { get; set; }
+        [ForeignKey("Course")]
+        public int course_id { get; set; }
+        public Course? Course { get; set; }
+        public bool status { get; set; }
+    }
+}
