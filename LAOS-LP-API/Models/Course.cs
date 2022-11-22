abdf52cde20846abd03df4a88d5d545637cc39e6
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-22 15:43:08 +0700

    create course model

---
+++ LAOS-LP-API/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LAOS_LP_API.Models
+{
+    public class Course
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string image { get; set; }
+        public string description { get; set; }
+        [ForeignKey("Category")]
+        public int category_id { get; set; }
+        public virtual Category? Category { get; set; }
+        public int price { get; set; }
+    }
+}
