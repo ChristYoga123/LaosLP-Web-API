b2ba281efe2468e221f48a17e8c49398f57256bf
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-22 15:45:10 +0700

    create lesson model

---
+++ LAOS-LP-API/Models/Lesson.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LAOS_LP_API.Models
+{
+    public class Lesson
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+        [ForeignKey("Course")]
+        public int course_id { get; set; }
+        public Course? Course { get; set; }
+    }
+}
