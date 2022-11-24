458acc6e66c3e1420293ef53de7de10ea28156ca
Christianus Yoga Wibisono <93587043+ChristYoga123@users.noreply.github.com>
2022-11-25 00:22:12 +0700

    create jwt login

--- LAOS-LP-API/Controllers/UserController.cs
+++ LAOS-LP-API/Controllers/UserController.cs
@@ -3,6 +3,12 @@ using LAOS_LP_API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
 namespace LAOS_LP_API.Controllers
 {
@@ -10,9 +16,14 @@ namespace LAOS_LP_API.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        public IConfiguration _configuration;
         private readonly ApplicationDbContext _context;
 
-        public UserController(ApplicationDbContext context) => _context = context;
+        public UserController(IConfiguration config, ApplicationDbContext context)
+        {
+            _configuration = config;
+            _context = context;
+        } 
 
         [HttpGet("id")]
         public IActionResult Show(int id)
@@ -28,9 +39,29 @@ namespace LAOS_LP_API.Controllers
             var usr = _context.users.Where(u => u.email.Equals(login.email) && u.password.Equals(login.password)).FirstOrDefault();
             if (usr != null)
             {
-                return Ok("Login Sukses");
+                var claims = new[]
+                {
+                   new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                   new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                   new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                   new Claim("id", usr.id.ToString()),
+                   new Claim("name", usr.name.ToString()),
+                   new Claim("email", usr.email.ToString()),
+                   new Claim("password", usr.password.ToString()),
+                   new Claim("is_admin", usr.is_admin.ToString()),
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var token = new JwtSecurityToken(
+                        _configuration["Jwt:Issuer"],
+                        _configuration["Jwt:Audience"],
+                        claims,
+                        expires: DateTime.UtcNow.AddMinutes(10),
+                        signingCredentials: signIn);
+                return Ok(new JwtSecurityTokenHandler().WriteToken(token));
             }
-            return NotFound("Username atau Password salah");
+            return BadRequest("Username atau Password salah");
         }
 
         [HttpPost]
--- LAOS-LP-API/Program.cs
+++ LAOS-LP-API/Program.cs
@@ -1,5 +1,8 @@
 using LAOS_LP_API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +14,19 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("LaosDb")));
 builder.Services.AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +37,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
